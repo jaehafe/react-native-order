74e00fee79b70217db306d5b68b4b37ba2d438f1
Jaeha Lee <wogkdkrm112@gmail.com>
2024-02-12 22:04:07 +0900

    feat: expo-image-picker 라이브러리 추가 및 기능 추가

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,15 @@
-import Button from '@/components/@common/Button';
 import * as React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import Button from '@/components/@common/Button';
+import Colors from '@/constants/Colors';
+import { defaultImage } from '@/components/ProductListItem';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreateProductScreen() {
   const [name, setName] = React.useState('');
   const [price, setPrice] = React.useState('');
   const [errors, setErrors] = React.useState('');
+  const [image, setImage] = React.useState<string | null>(null);
 
   const resetFields = () => {
     setName('');
@@ -41,8 +45,28 @@ export default function CreateProductScreen() {
     resetFields();
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Image source={{ uri: image || defaultImage }} style={styles.image} />
+      <Text style={styles.textButton} onPress={pickImage}>
+        Select Image
+      </Text>
       <Text style={styles.label}>Name</Text>
       <TextInput value={name} onChangeText={setName} placeholder="Name" style={styles.input} />
 
@@ -60,6 +84,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 10,
+  },
+  textButton: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+    alignSelf: 'center',
   },
   label: {
     color: 'gray',
